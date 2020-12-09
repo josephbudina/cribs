0aac8b36dd61706b3f1b4fb8785d0fdbb03caad2
Joseph Budina <joebud3@gmail.com>
2020-12-09 13:15:27 -0700

    Add house exists test

---
+++ test/house_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+require './lib/house'
+
+class HouseTest < Minitest::Test
+  def test_it_exists_with_attributes
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_instance_of House, house
+    assert_equal 400000, house.price
+    assert_equal "123 sugar lane", house.address
+    assert_equal [], house.rooms
+  end
+end
