85829092e7e762328bbf7783c39fe46e2ee8bc73
Joseph Budina <joebud3@gmail.com>
2020-12-09 13:11:11 -0700

    Fix tests and add paint room test

--- test/room_test.rb
+++ test/room_test.rb
@@ -9,16 +9,26 @@ class RoomTest < Minitest::Test
   end
 
   def test_it_has_a_category
-    skip
+    # skip
     room = Room.new(:bedroom, 10, '13')
     assert_equal :bedroom, room.category
   end
 
   def test_it_can_get_area
-    skip
+    # skip
     room1 = Room.new(:bedroom, 10, '13')
-    room2 = Room.new(:living_room, 15, '12')
+    room2 = Room.new(:living_room, 15, '18')
     assert_equal 130, room1.area
     assert_equal 180, room2.area
+  end
+
+  def test_it_can_paint_room
+    room1 = Room.new(:bedroom, 10, '13')
+
+    assert_equal false, room1.is_painted?
+
+    room1.paint
+
+    assert_equal true, room1.is_painted?
   end
 end
