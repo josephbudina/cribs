313199fafdd8fa82a92c31df2eb91e8fb3360cf0
Joseph Budina <joebud3@gmail.com>
2020-12-09 13:19:19 -0700

    Initialize house with attributes

---
+++ lib/house.rb
@@ -0,0 +1,15 @@
+class House
+  attr_reader :price,
+              :address,
+              :rooms
+
+  def initialize(price, address)
+    @price = price
+    @address = address
+    @rooms = []
+  end
+
+  def price
+    @price[1..-1].to_i
+  end
+end
